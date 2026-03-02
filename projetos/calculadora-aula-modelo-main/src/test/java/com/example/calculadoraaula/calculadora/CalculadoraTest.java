0fb3b8265b92cea8fc2115dc107ba5bf4aadecd0
brunoqp78 <brunoqp78@gmail.com>
2026-03-02 14:52:05 -0300

    aula 23 de fevereiro

--- projetos/calculadora-aula-modelo-main/src/test/java/com/example/calculadoraaula/calculadora/CalculadoraTest.java
+++ projetos/calculadora-aula-modelo-main/src/test/java/com/example/calculadoraaula/calculadora/CalculadoraTest.java
@@ -6,8 +6,33 @@ import org.junit.jupiter.api.Test;
 public class CalculadoraTest {
 
     @Test
-    public void teste1(){
-        Assertions.assertEquals(5, (new Calculadora()).somar(2, 3));
+    public void testeSomaNumerosPositivos(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 2;
+        int entradaNumero2 = 3;
+        int resultadoEsperado = 5;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.somar(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }
+
+    @Test
+    public void testeSubtrairNumerosPositivos(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 5;
+        int entradaNumero2 = 3;
+        int resultadoEsperado = 2;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.subtrair(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
     }
 
 }
