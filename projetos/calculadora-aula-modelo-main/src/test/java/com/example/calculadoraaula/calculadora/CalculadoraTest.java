0249484ae49d367a314941878d3c4bff0f96e306
brunoqp78 <brunoqp78@gmail.com>
2026-03-02 15:02:05 -0300

    Preenchendo todos os métodos da classe de teste.

--- projetos/calculadora-aula-modelo-main/src/test/java/com/example/calculadoraaula/calculadora/CalculadoraTest.java
+++ projetos/calculadora-aula-modelo-main/src/test/java/com/example/calculadoraaula/calculadora/CalculadoraTest.java
@@ -20,6 +20,21 @@ public class CalculadoraTest {
         Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
     }
 
+     @Test
+    public void testeSomaNumerosPositivoNegativo(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 5;
+        int entradaNumero2 = -3;
+        int resultadoEsperado = 2;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.somar(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }
+
     @Test
     public void testeSubtrairNumerosPositivos(){
         //arrange
@@ -34,5 +49,96 @@ public class CalculadoraTest {
         //Assert
         Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
     }
+
+    @Test
+    public void testeSubtrairUmNumeroNegativo(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 3;
+        int entradaNumero2 = -2;
+        int resultadoEsperado = 5;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.subtrair(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }    
+
+    @Test
+    public void testeMultiplicarNumerosPositivos(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 2;
+        int entradaNumero2 = 3;
+        int resultadoEsperado = 6;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.multiplicar(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }
+
+    @Test
+    public void testeMultiplicarPorZero(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 5;
+        int entradaNumero2 = 0;
+        int resultadoEsperado = 0;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.multiplicar(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }
+
+    @Test
+    public void testeDividirSemGerarResto(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 6;
+        int entradaNumero2 = 3;
+        int resultadoEsperado = 2;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.dividir(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }
+
+    @Test
+    public void testeDividirGerandoResto(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 5;
+        int entradaNumero2 = 2;
+        int resultadoEsperado = 2;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.dividir(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }   
+    
+    @Test
+    public void testeDividirPorZero(){
+        //arrange
+        Calculadora calculadoraTestada = new Calculadora();
+        int entradaNumero1 = 5;
+        int entradaNumero2 = 0;
+        int resultadoEsperado = -1;
+
+        //Act
+        int resultadoRealObtido = calculadoraTestada.dividir(entradaNumero1, entradaNumero2);
+
+        //Assert
+        Assertions.assertEquals(resultadoEsperado, resultadoRealObtido);
+    }   
+    
 
 }
